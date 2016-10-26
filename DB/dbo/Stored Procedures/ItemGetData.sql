6e33c234fd58154688a4f6fda6b9d41595159a3e
buddhirameshi <buddhirameshi@gmail.com>
2016-10-25 22:03:15 -0400

    Added a DB Project

---
+++ DB/dbo/Stored Procedures/ItemGetData.sql
@@ -0,0 +1,35 @@
+﻿-- =============================================
+-- =============================================
+CREATE PROCEDURE [dbo].[ItemGetData] 
+(
+	@filter varchar(max) ='',
+	@sort varchar(30)='ItemId',
+	@isSortDirAsc bit=0
+)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	SET @sort = QUOTENAME(@sort);
+	DECLARE @sql NVARCHAR(MAX),
+	@sortDir NVARCHAR(5);
+
+	IF(@isSortDirAsc=0)
+	SET @sortDir='DESC'
+	ELSE
+	SET @sortDir='ASC'
+
+  IF(@filter!='')
+  SET @sql = N'SELECT TOP 10000 ItemId,Price,Description
+               FROM dbo.Item
+			   WHERE Description LIKE % '+@filter+'%) 
+               ORDER BY ' + @sort +@sortDir+ ';';
+ELSE
+  SET @sql = N'SELECT TOP 10000 ItemId,Price,Description
+               FROM dbo.Item
+               ORDER BY '  + @sort +@sortDir+';';
+
+
+  EXEC sp_executesql @sql;
+END
---
+++ DB/dbo/Stored Procedures/ItemGetTotal.sql
@@ -0,0 +1,17 @@
+﻿-- =============================================
+-- =============================================
+CREATE  PROCEDURE [dbo].[ItemGetTotal] 
+(
+	@filter varchar(max) =''
+)
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    IF(@filter!='')
+				SELECT COUNT(*) FROM Item  WHERE Description LIKE ('%'+@filter+'%');
+	ELSE
+				SELECT COUNT(*) FROM Item 
+END
